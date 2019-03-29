77912fe8016548c66c2c50be4011d8adb5a4bbb7
mozheng <daniuatchina@163.com>
2019-03-29 20:24:06 +0800

    添加老婆爬虫器

--- Cat_or_Dog/test.py
+++ Cat_or_Dog/test.py
@@ -1,9 +1,5 @@
-import os
-import random
+#!/usr/bin/env python
+# coding=utf8
 
 
 
-path="D:/Data/DogsvsCats/train"
-make_list_file(path)
-a = load_and_shuffle_images_listfile("train.txt")
-print(a)
